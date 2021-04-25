1307821a3b4b98130e591a2e984006e1125074fb
Ardash <ardache@hotmail.com>
2021-04-25 08:21:44 -0500

    stats an insert update

--- js/demo/daysleft.js
+++ js/demo/daysleft.js
@@ -3,6 +3,35 @@ var porcTerTrim = 0
 var porcTotalEmbarazo = 0
 var dt2 = new Date('4/29/2021');
 var dt1 = new Date(Date.now())
+
+
+
+var saldoActual = "0,500"
+document.getElementById("saldoActual").innerHTML = saldoActual
+
+
+var actualLevel = "Nivel 4-2"
+var pastLevel="Nivel 4-1"
+var acumulado = 200;
+document.getElementById("actualLevel").innerHTML = actualLevel
+document.getElementById("pastLevel").innerHTML = pastLevel
+document.getElementById("acumulado").innerHTML = acumulado
+
+// 750 stats
+var resto750 = "5 meses, para completar 109,000 puntos"
+document.getElementById("resto750").innerHTML =  resto750
+document.getElementById("porc750").innerHTML = "0%"
+document.getElementById("totalBar750").style.width = "0%"
+
+// TR4 stats
+//var restoTR4 = "Faltan aprox. 8 meses para completar"
+var restoTR4 = "Faltan 4 meses por iniciar o menos"
+document.getElementById("restoTR4").innerHTML =  restoTR4
+document.getElementById("porcTR4").innerHTML = "0%"
+document.getElementById("totalBarTR4").style.width = "0%"
+
+
+
 console.log(dt1)
 console.log(dt2)
 var diffDays = Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24)); 
@@ -17,6 +46,7 @@ document.getElementById("totalSemanas").innerHTML =  Math.floor((diffDays)) + " 
 document.getElementById("totalBar").style.width = porcTotalEmbarazo + "%"
 document.getElementById("porc750").innerHTML = "0%"
 document.getElementById("totalBar750").style.width = "0%"
+
 
 
 
